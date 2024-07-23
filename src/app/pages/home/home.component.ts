7b1dfafef176045e6849337ac1df62a3d8e776ea
dcwebcode <dcastiblanque@educa.madrid.org>
2024-07-24 00:45:12 +0200

    Create presentation component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 import { PresentationComponent } from '../../shared/components/presentation/presentation.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
